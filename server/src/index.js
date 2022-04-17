05a96742f1c2c2fb8301afd3733dea965f17d7b7
Matteo Facci <36844147+faccimatteo@users.noreply.github.com>
2022-04-17 18:18:16 +0200

    Save only

--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,10 @@ const multer = require("multer");
 const path = require("path");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
-
+const redis = require('redis');
 const Queue = require('bull');
+const fs = require('fs');
+const crypto = require('crypto');
 
 const main = async () => {
     const app = express();
@@ -17,6 +19,15 @@ const main = async () => {
             port: 3002,
         }
     });
+    
+    const redisClient = redis.createClient({
+        url: 'redis://localhost:3002'
+    });
+
+    redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+    await redisClient.connect();
+
     const numWorkers = 4;
     waitingQueue.process(numWorkers, __dirname + "/processor.js");
     waitingQueue.on('failed', (job, result) => {
@@ -76,23 +87,71 @@ const main = async () => {
             } else {
                 serverFilename = req.files["upload-area"][0].filename;
                 executableFilename = path.basename(serverFilename,path.extname(serverFilename));
-                // Absolutes path: req.files["upload-area"][0].path
-                const job = await waitingQueue.add({
-                    serverFilename: serverFilename,
-                    executableFilename: executableFilename
-                })
+                async function takeFromCache() {
+                    fs.readFile(req.files["upload-area"][0].path, (err, data) => {
+                        programHash = crypto.createHash('sha512').update(data.toString()).digest('hex');
+                        cachedProgram = redisClient.hGetAll(programHash).then(
+                            (content) => {
+                                if (content.program_name !== undefined) {   
+                                    exec(`rm ./uploads/${serverFilename}`);
+                                    res.download(
+                                        path.join(__dirname, `../uploads/${content.program_name}`),
+                                        content.program_name.replace(/-\d+$/, ""), {
+                                            headers: {
+                                                filename: content.program_name.replace(/-\d+$/, ""),
+                                                success: "true",
+                                            }, 
+                                        }
+                                        
+                                    );
+                                } else {
+                                    compileProgram();
+                                }
+                            }
+                        );
+                    })
+                };
+
+                async function compileProgram() {
+                    // Absolutes path: req.files["upload-area"][0].path
+                    const job = await waitingQueue.add({
+                        serverFilename: serverFilename,
+                        executableFilename: executableFilename
+                    })
+                    
+                    const result = await job.finished();
+                    if(result.error) {
+                        res.send(result)
+                    } else {
+
+                        fs.readFile(req.files["upload-area"][0].path, (err, data) => {
+                            if (data) {
+                                programHash = crypto.createHash('sha512').update(data.toString()).digest('hex');
+                                redisClient.hSet(programHash, "program_name", executableFilename).then(
+                                    (content) => {
+                                        res.download(
+                                            path.join(__dirname, `../uploads/${executableFilename}`),
+                                            executableFilename.replace(/-\d+$/, ""),
+                                            result
+                                        );
+                                    }
+                                );
+                            }
+                        });
+
+                    }
+                };
+
+                const downloadPromise = new Promise(function(downloadCallback) {
+                    downloadCallback();
+                });
                 
-                const result = await job.finished();
-                if(result.error) {
-                    res.send(result)
-                } else {
-                    res.download(
-                        path.join(__dirname, `../uploads/${executableFilename}`),
-                        executableFilename.replace(/-\d+$/, ""),
-                        result
-                    );
-                }
-                await exec(`rm ./uploads/"${serverFilename}" ${result.error === 1002 ? "" : `./uploads/"${executableFilename}"`}`);
+                downloadPromise.then(() => {
+                    takeFromCache();
+                });
+                
+                //await exec(`rm ./uploads/${serverFilename}`);
+                //${result.error === 1002 ? "" : `./uploads/"${executableFilename}"`}`);
             }
         });
     });
--- server/src/processor.js
+++ server/src/processor.js
@@ -1,10 +1,49 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const path = require("path");
+const redis = require('redis');
+const crypto = require('crypto');
+const fs = require('fs');
+
+const MAXCACHEDELEMENTS = 2; 
 
 module.exports = async function (job) {
     const { serverFilename, executableFilename } = job.data
+
+    const redisClient = redis.createClient({
+        url: 'redis://localhost:3002'
+    });
+
+    redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+    await redisClient.connect();    
+
     try {
-        let { error, stdout, stderr } = await exec(`g++ -o ./uploads/"${executableFilename}" ./uploads/"${serverFilename}"`);
+        let { error, stdout, stderr } = await exec(`ls ./uploads | grep -v .cc | grep -v .cpp | wc -l `);
+        // if max element of executable is reached
+        let executableNumber = stdout.replace(/\s/g, '');
+        
+        if (executableNumber == MAXCACHEDELEMENTS ) {
+            // getting older executable
+            ({ error, stdout, stderr } = await exec(`ls -t ./uploads | grep -v .cc | grep -v .cpp | tail -1`));
+           
+            // removing older executable 
+            await exec(`rm ./uploads/${stdout}`);
+            ({error, stdout, stderr} = await exec(`ls ./uploads | grep ${stdout}`)); // return source of the older exec 
+            let filePos = path.join(__dirname, `../uploads/${stdout}`);
+
+            // once we delete the excutable we need to remove the hash associated in redis
+            fs.readFile(filePos.slice(0,-1), (err, data) => {
+                programHash = crypto.createHash('sha512').update(data.toString()).digest('hex');
+                exec(`docker ps -q -f "name=redis-waiting-queue" | xargs -o -I'{}' docker exec -it {} redis-cli HDEL ${programHash} program_name`);
+                console.log(programHash + " :content deleted from the cache");
+                exec(`rm ${filePos}`); // removing source since we don't need the file anymore
+            });
+        }
+
+        ({error, stdout, stderr} = await exec(`g++ -o ./uploads/"${executableFilename}" ./uploads/"${serverFilename}"`));
+        
+        
         // If the file compiled without any error, stout|stderr|error should be empty
     } catch (error) {
         return Promise.resolve({
